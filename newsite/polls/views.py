6ed26d82e634e6730225a8b07f01b5619f611abc
d.bagaryan@devcaz.com <d.bagaryan@devcaz.com>
2018-12-14 19:13:03 +0100

    new functionality for rendering templatest and views management

--- newsite/polls/views.py
+++ newsite/polls/views.py
@@ -1,33 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
-from django.template import loader
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpRequest
 
 from .models import Question
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    latest_question_list = Question.objects.all()[:5]
-    template = loader.get_template('polls/index.html')
+def index(request: HttpRequest):
+    latest_question_list = get_list_or_404(Question)[:5]  # Question.objects.all()[:5]
     context = {'title': 'hello index', 'latest_question_list': latest_question_list}
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
-def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    resp_str = f'You\'re looking at question {question_id}'
-    template = loader.get_template('polls/detail.html')
-    context = {'title': f'{question_id} details', 'resp_str': resp_str}
+def detail(request: HttpRequest, question_id: int):
+    question = get_object_or_404(Question, pk=question_id)
 
-    return HttpResponse(template.render(context, request))
+    # try:
+    #     question = Question.objects.get(id=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404(f'Question {question_id} not found')
+
+    # resp_str = f"You're looking at question {question_id}"
+    # context = {'title': f'{question_id} details', 'resp_str': resp_str}
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request: HttpRequest, question_id: int) -> HttpResponse:
-    # if not isinstance(question_id, str):
-    #     raise Exception('common MFK!')
-
+def results(request: HttpRequest, question_id: int):
     response = f"You're looking at the results of question {question_id}"
-    return HttpResponse(response)
+    context = {'title': f'{question_id} result', 'resp_str': response}
+
+    return render(request, 'polls/result.html', context)
 
 
-def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(f"You're voting on question {question_id}")
+def vote(request: HttpRequest, question_id: int):
+    response = f"You're voting on question {question_id}"
+    context = {'title': f'{question_id} vote', 'resp_str': response}
+
+    return render(request, 'polls/vote.html', context)
