86bb19c89a705833709899ab1b186b4200491abd
d.bagaryan@devcaz.com <d.bagaryan@devcaz.com>
2018-12-14 17:05:32 +0100

    some refactoring

--- newsite/polls/views.py
+++ newsite/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.template import loader
+
+from .models import Question
 
 
-def index(request: HttpRequest, lol: str, test: str) -> HttpResponse:
-    return HttpResponse('Hello Julia from index!' + lol + test)
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.all()[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'title': 'hello index', 'latest_question_list': latest_question_list}
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
