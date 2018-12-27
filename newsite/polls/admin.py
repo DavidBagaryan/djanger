2ff206140ceda2f62b0aa10ee47f92077a21c1c1
David Bagarian <david.bagaryan@mail.ru>
2018-12-28 01:09:27 +0300

    completed first parts of test app

--- newsite/polls/admin.py
+++ newsite/polls/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+admin.AdminSite.site_header = 'POLLS ADMIN MFKRS |-{{ AWESOME'
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
